aa8a8da081d2caffadb11125adea18e26897c542
shahraiz-cb <shahraiz.ali@crowdbotics.com>
2023-03-23 23:38:16 +0500

    [Crowdbotics Platform Commit]
    Crowdbotics Studio: Update app screens

--- options/options.js
+++ options/options.js
@@ -2,7 +2,7 @@ export const globalOptions = {
   "name": "platform-demo-011-39525",
   "url": "https://platform-demo-011-39525.botics.co",
   "api": "https://platform-demo-011-39525.botics.co/api/v1",
-  "initialRoute": "many-rows"
+  "initialRoute": "Untitled1"
 };
 export const modulesOptions = {
   "@modules/app-menu": {
--- screens/Untitled1/index.js
+++ screens/Untitled1/index.js
@@ -1,15 +1,20 @@
+import { useNavigation } from "@react-navigation/native";
+import { Pressable } from "react-native";
 import { ImageBackground } from "react-native";
 import React from "react";
 import { StyleSheet, ScrollView, SafeAreaView } from "react-native";
 
 const Untitled1 = () => {
+  const navigation = useNavigation();
   return <SafeAreaView style={styles.safeArea}>
         <ScrollView contentContainerStyle={{
       backgroundColor: '#E45700',
       padding: 10,
       position: 'relative',
       flex: 1
-    }}><ImageBackground style={styles.IJBzqiGP} source={require("./Recipeful(2).png")} resizeMode="cover"></ImageBackground></ScrollView>
+    }}><Pressable onPress={() => {
+        navigation.navigate("manyRows", {});
+      }}><ImageBackground style={styles.IJBzqiGP} source={require("./Recipeful(2).png")} resizeMode="cover"></ImageBackground></Pressable></ScrollView>
     </SafeAreaView>;
 };
 
@@ -21,8 +26,8 @@ const styles = StyleSheet.create({
     width: 359,
     height: 243,
     position: "absolute",
-    left: -2,
-    top: 75
+    left: -10,
+    top: 97
   }
 });
 export default Untitled1;
